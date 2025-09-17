89f8d2232a72453de70ff0b78a92a521ed712281
patchaploy <patchaploy.ana@student.mahidol.edu>
2025-09-17 21:33:03 +0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -13,7 +13,7 @@ import pandas as pd
 import numpy as np
 import datetime
 import gradio as gr
-from .data_manager import user_health_data, save_data, _hash_password
+from data_manager import user_health_data, save_data, _hash_password
 
 # --- Calculation Helper Functions ---
 
